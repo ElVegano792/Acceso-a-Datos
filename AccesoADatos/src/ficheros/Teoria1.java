492ef8f2ca5d4fdef9aa638369699380b12ade45
alumno <alumno@2DAM-16>
2026-09-16 13:58:48 +0200

    sadsadwa

--- AccesoADatos/src/ficheros/Teoria1.java
+++ AccesoADatos/src/ficheros/Teoria1.java
@@ -3,10 +3,30 @@ package ficheros;
 import java.io.File;
 
 public class Teoria1 {
-
-	public static void main(String[] args) {
+	
+	//private static final String DIR_CONFIG = "DAM2/martin";
+	private static final String DIR_CONFIG = "DAM2"; // Solo puede crear un directorio a la vez, se puede spamear para crearlo todo
+	
+	public static void main(String[] args) {	
 		
 		File directorioActual = new File(".");
+		System.out.println(directorioActual.getAbsolutePath()); // Con esto obtenemos la ubicación actual del directorio
+		
+		// Vv Con esto y con el private static de arriba le decimos la ubcación del archivo que nosotros querramos vV \\
+		File dirConfig = new File(DIR_CONFIG);
+		System.out.println(dirConfig);
+		
+		// Esto es un metodo para comprobar si existe o no ese directorio
+		if(dirConfig.exists() == true) {
+			System.out.println("El directorio " + DIR_CONFIG + " existe.");
+		}
+		else {
+			System.out.println("El directorio " + DIR_CONFIG + " no existe.");
+			dirConfig.mkdir(); // Y con esto lo creamos en linux
+			System.out.println("Creando directorio " + DIR_CONFIG + ".");
+		}
+		
+		
 		
 	}
 
