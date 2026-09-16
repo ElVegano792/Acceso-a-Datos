88f186bfef636bf25486afe97b92e3c67ff240b6
alumno <alumno@2DAM-16>
2026-09-16 14:18:38 +0200

    asdasd

--- AccesoADatos/src/ficheros/Teoria1.java
+++ AccesoADatos/src/ficheros/Teoria1.java
@@ -5,13 +5,13 @@ import java.io.File;
 public class Teoria1 {
 	
 	//private static final String DIR_CONFIG = "DAM2/martin";
-	private static final String DIR_CONFIG = "DAM2"; // Solo puede crear un directorio a la vez, se puede spamear para crearlo todo
-	//private static final String DIR_CONFIG = "DAM2"+File.separator+"martin"; // Con esto nos aseguramos de que se pueda hacer tanto en linux como en windows
+//	private static final String DIR_CONFIG = "DAM2"; // Solo puede crear un directorio a la vez, se puede spamear para crearlo todo
+	private static final String DIR_CONFIG = "DAM2"+File.separator+"martin"; // Con esto nos aseguramos de que se pueda hacer tanto en linux como en windows
 	
-	public static void main(String[] args) {	
+	public static void main(String[] args) {
 		
 		File directorioActual = new File(".");
-		System.out.println(directorioActual.getAbsolutePath()); // Con esto obtenemos la ubicación actual del directorio
+//		System.out.println(directorioActual.getAbsolutePath()); // Con esto obtenemos la ubicación actual del directorio
 		
 		// Vv Con esto y con el private static de arriba le decimos la ubcación del archivo que nosotros querramos vV \\
 		File dirConfig = new File(DIR_CONFIG);
@@ -27,8 +27,14 @@ public class Teoria1 {
 			System.out.println("Creando directorio " + DIR_CONFIG + ".");
 		}
 		
-		
+		File directorioConfig = new File(DIR_CONFIG+File.separator+"config.txt");
+		if(directorioConfig.exists() == true) {
+			System.out.println("El archivo ya existe");
+		} else {
+			System.out.println("El archivo no existe");
+			
+		}
 		
 	}
-
+	
 }
