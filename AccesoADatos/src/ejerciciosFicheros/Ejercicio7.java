374262c6106d39c0c464734878dc2ac2c12d4db0
Martin <czbarabaja75@gmail.com>
2026-09-24 10:41:52 +0200

    sadasd

---
+++ AccesoADatos/src/ejerciciosFicheros/Ejercicio7.java
@@ -0,0 +1,32 @@
+package ejerciciosFicheros;
+
+import java.io.DataInputStream;
+import java.io.FileInputStream;
+
+public class Ejercicio7 {
+	
+	final static String ruta = "coordenadas.dat";
+	
+	
+	public static void main(String[] args) {
+		final int NUM_REGISTROS = 3;
+		try(DataInputStream fichero = new DataInputStream(new FileInputStream(ruta))){
+			
+			System.out.println("SATELITES Y COORDENADAS");
+			for(int i = 0;i< NUM_REGISTROS;i++) {
+				int id = fichero.readInt();
+				float latitud = fichero.readFloat();
+				float longitud = fichero.readFloat();
+				String estado = "";
+				for (int j=0;j<4;j++) {
+					estado+=fichero.readChar();
+				}
+				System.out.printf("Satélite ID: %d | Posición: (%.4f, %.4f) | Estado: %s \n",id,latitud,longitud,estado);
+			}
+			
+		} catch (Exception e) {
+			System.out.println("Error al leer el fichero.");
+		}
+		
+	}	
+}
